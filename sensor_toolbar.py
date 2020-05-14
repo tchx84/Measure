c8f8c99acaab587ffc4c4af4f82d6d03c3e629e6
Martin Abente Lahaye <tch@sugarlabs.org>
2020-05-14 09:45:38 -0400

    sensor_toolbar: Fix crash when switching trigger controls
    Measure will crash when the user:
    1. Switches to Time Base.
    2. Clickes on Rising Edge.
    
    This issue was somewhat handled by catching RecursionError
    but is not always reliable, see https://bugs.python.org/issue25222
    
    I am don't know why the recursion needs to happen in the first
    place, but an simpler fix is just to prevent it from happening
    unnecessarily.

--- sensor_toolbar.py
+++ sensor_toolbar.py
@@ -85,6 +85,7 @@ of XO)") + ' '
         self.mic_boost = MIC_BOOST
 
         self.mode = 'sound'
+        self._current_trigger_control = None
 
         # Set up Time-domain Button
         self.time = RadioToolButton(icon_name='media-audio', group=None)
@@ -250,13 +251,13 @@ of XO)") + ' '
     def update_trigger_control_cb(self, button, value):
         if button is None:
             value = self.activity.wave.TRIGGER_NONE
-        try:
-            if self.activity.wave.get_fft_mode():
-                self.trigger_none.set_active(True)
-            else:
-                self.activity.wave.set_trigger(value)
-        except RuntimeError as e:
-            logging.debug('Warning: Maximum Recursions on the same method has taken place', e)
+        if self._current_trigger_control == value:
+            return
+        self._current_trigger_control = value
+        if self.activity.wave.get_fft_mode():
+            self.trigger_none.set_active(True)
+        else:
+            self.activity.wave.set_trigger(value)
 
     def analog_resistance_voltage_mode_cb(self, button=None,
                                           mode_to_set='sound'):
